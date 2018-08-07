a8f7b5b66501d5364692cfae109a643b4438ba97
Jose Carlos Montero Gómez <sipox11@gmail.com>
2018-08-07 09:31:03 +0200

    Add init scripts for production and development

--- src/app.js
+++ src/app.js
@@ -2,5 +2,6 @@ const express = require('express');
 const app = express();
 
 app.listen(3000, () => {
+    // Start it with node src/app.js
     console.log('Server listening on port 3000...');
 })
