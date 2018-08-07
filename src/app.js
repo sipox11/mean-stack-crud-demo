86de1cede242ad644b0aa492a24ae3075a8baed1
Jose Carlos Montero Gómez <sipox11@gmail.com>
2018-08-07 09:47:33 +0200

    Link routes file

--- src/app.js
+++ src/app.js
@@ -1,7 +1,28 @@
+const path = require('path');
 const express = require('express');
+const morgan = require('morgan');
 const app = express();
 
-app.listen(3000, () => {
+// Importing routes
+const indexRoutes = require('./routes/index');
+
+// Settings
+app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+// Middleware
+// -- To verbose the request log
+app.use(morgan('dev')); 
+// -- To understand the data received from forms
+app.use(express.urlencoded({extended: false})); 
+
+// Routes
+app.use('/', indexRoutes);
+
+
+// Start the server
+app.listen(app.get('port'), () => {
     // Start it with node src/app.js
-    console.log('Server listening on port 3000...');
+    console.log(`Server listening on port ${app.get('port')}...`);
 })
